ec962e39b639d1ce7f451e225de1fbae85d64611
Jason Woofenden <jason183@herkamire.com>
2006-12-31 05:07:53 -0500

    metaform: added int, hidden and password to list of available field types added copyright notice to session.php

--- session.php
+++ session.php
@@ -1,5 +1,24 @@
 <?php
 
+#  Copyright (C) 2006 Jason Woofenden
+#
+#  This file is part of wfpl.
+#
+#  wfpl is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  wfpl is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with wfpl; see the file COPYING.  If not, write to the
+#  Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+#  MA 02111-1307, USA.
+
 # you'll need this file that calles db_connect()
 require_once('db_connect.php');
 
